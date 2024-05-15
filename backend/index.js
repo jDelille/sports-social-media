41fb6ea4dd6c35b56a1d859925b995f1f327c0aa
jDelille <justind7513@gmail.com>
2024-05-14 17:13:11 -0700

    setting up project

---
+++ backend/index.js
@@ -0,0 +1,39 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import multer from "multer";
+const app = express();
+
+// middlewares
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
+});
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
+app.use(cookieParser());
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "../frontend/public/upload");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  const file = req.file;
+  res.status(200).json(file.filename);
+});
+
+app.listen(8800, () => {
+  console.log("backend working.");
+});
