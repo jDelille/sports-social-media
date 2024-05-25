e8b8311377933c10535435626a4d0881ffa9ccf8
jDelille <justind7513@gmail.com>
2024-05-25 12:49:26 -0700

    feat: added react-router-dom and layout

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useContext, useState } from "react";
+import useLoginModal from "../hooks/useLoginModal";
+import useRegisterModal from "../hooks/useRegisterModal";
+import { useAxios } from "../hooks/useAxios";
+import { AuthContext } from "../context/AuthContext";
 
-type HomeProps = {
- 
- }
+type HomeProps = {};
 const Home: React.FC<HomeProps> = () => {
+  const loginModal = useLoginModal();
+  const registerModal = useRegisterModal();
+  const [body, setBody] = useState("Testing body from react");
+  const { currentUser } = useContext(AuthContext) || {};
+
+  const postData = {
+    body,
+  };
+  const createPost = async () => {
+    return useAxios.post("/posts", postData);
+  };
+
   return (
-    <div className='home-page'>
-      
+    <div className="home-page">
+      {currentUser ? (
+        <button onClick={createPost}>Create post</button>
+      ) : (
+        <>
+          <button onClick={loginModal.onOpen}>Login</button>
+          <button onClick={registerModal.onOpen}>Register</button>
+        </>
+      )}
     </div>
   );
 };
