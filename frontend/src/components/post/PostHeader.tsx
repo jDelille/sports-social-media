e6fb962e89c16b876a6c76b788e2b84b2bc161e5
jDelille <justind7513@gmail.com>
2024-05-30 22:15:49 -0700

    feat: added moment

--- frontend/src/components/post/PostHeader.tsx
+++ frontend/src/components/post/PostHeader.tsx
@@ -1,11 +1,36 @@
 import React from 'react';
 import UserTypes from '../../types/User';
+import PostTypes from '../../types/Post';
+import moment from "moment";
 
 type PostHeaderProps = {
     user: UserTypes;
+    post: PostTypes;
  }
 
-const PostHeader: React.FC<PostHeaderProps> = ({user}) => {
+const PostHeader: React.FC<PostHeaderProps> = ({user, post}) => {
+
+  moment.updateLocale("en", {
+    relativeTime: {
+      future: "in %s",
+      past: "%s",
+      s: "1s",
+      ss: "%ds",
+      m: "1m",
+      mm: "%dm",
+      h: "1hr",
+      hh: "%dhr",
+      d: "1d",
+      dd: "%dd",
+      w: "1w",
+      ww: "%dw",
+      M: "1mo",
+      MM: "%dmo",
+      y: "1yr",
+      yy: "%dyrs",
+    },
+  });
+
   return (
     <div className='post-header'>
       <div className="avatar">
@@ -14,6 +39,9 @@ const PostHeader: React.FC<PostHeaderProps> = ({user}) => {
       <div className="user">
         <strong>{user.name}</strong>
         <span>{user.username}</span>
+      </div>
+      <div className="details">
+        <span>{moment(post.created_at).fromNow()}</span>
       </div>
     </div>
   );
