510af119b9501990d5eb20b34e7551bc9553d932
jDelille <justind7513@gmail.com>
2024-05-31 00:46:42 -0700

    feat: added comment hook

--- frontend/src/components/create-comment/CreateComment.tsx
+++ frontend/src/components/create-comment/CreateComment.tsx
@@ -2,50 +2,72 @@ import React, { useState } from "react";
 import Modal from "../modal/Modal";
 import { useAxios } from "../../hooks/useAxios";
 import useCreateCommentModal from "../../hooks/useCreateCommentModal";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
-type CreateCommentProps = {
- 
- }
+type CreateCommentProps = {};
 const CreateComment: React.FC<CreateCommentProps> = () => {
-    const [body, setBody] = useState("");
+  const queryClient = useQueryClient();
 
-    const createCommentModal = useCreateCommentModal();
+  const [body, setBody] = useState("");
 
-    const postId = createCommentModal.postId;
-    const type = createCommentModal.type;
+  const createCommentModal = useCreateCommentModal();
 
-    const handleSubmit = async () => {
-        try {
-            await useAxios.post("/comments", {
-                postId: postId,
-                body,
-                image: null,
-                type: type
-              });
-              
-              createCommentModal.onClose();
-        } catch (error) {
-            console.log(error)
-        }
-      };
+  const postId = createCommentModal.postId;
+  const type = createCommentModal.type;
 
-    const bodyContent = (
-        <div>
-          <input
-            type="text"
-            placeholder="Comment on this post"
-            onChange={(e) => setBody(e.target.value)}
-          />
-          <button onClick={handleSubmit}>Post</button>
-        </div>
-      );
+  const handleSubmit = async (body: string) => {
+    try {
+      await useAxios.post("/comments", {
+        postId: postId,
+        body,
+        image: null,
+        type: type,
+      });
 
-    const handleClose = () => {
-        createCommentModal.onClose();
-      };
+      createCommentModal.onClose();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const { mutate } = useMutation({
+    mutationFn: handleSubmit,
+    onSettled: async () => {
+      queryClient.refetchQueries();
+    },
+    mutationKey: ["addComment"],
+  });
+  
+  const handleSubmitClick = async () => {
+    try {
+      mutate(body);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+
+
+  const bodyContent = (
+    <div>
+      <input
+        type="text"
+        placeholder="Comment on this post"
+        onChange={(e) => setBody(e.target.value)}
+      />
+      <button onClick={handleSubmitClick}>Post</button>
+    </div>
+  );
+
+
+
+
+  const handleClose = () => {
+    createCommentModal.onClose();
+  };
 
   return (
-    <div className='create-comment'>
+    <div className="create-comment">
       <Modal
         body={bodyContent}
         isOpen={createCommentModal.isOpen}
