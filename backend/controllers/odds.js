0a1dc38353ded143364155b4f56b3ead1ab4a6f6
jDelille <justind7513@gmail.com>
2024-06-20 15:52:05 -0700

    feat: added sport picker

--- backend/controllers/odds.js
+++ backend/controllers/odds.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 export const getOdds = async (req, res) => {
-    // const { id } = req.params;
+    const {sport, league} = req.params
     try {
-        const response = await axios.get(`https://www.bovada.lv/services/sports/event/coupon/events/A/description/baseball/mlb`);
+        const response = await axios.get(`https://www.bovada.lv/services/sports/event/coupon/events/A/description/${sport}/${league}`);
         res.json(response.data[0].events);
     } catch (error) {
         console.error('Error fetching odds data:', error);
---
+++ frontend/src/components/sport-picker/SportPicker.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./sportPicker.scss";
+
+type Sport = {
+  sport: string;
+  league: string;
+};
+
+type SportPickerProps = {
+  onSportSelect: (sport: Sport) => void; 
+};
+
+const sports: Sport[] = [
+    { sport: 'hockey', league: 'nhl' },
+    // { sport: 'basketball', league: 'nba' },
+    { sport: 'football', league: 'nfl' },
+    { sport: 'baseball', league: 'mlb' },
+    // Add more sports as needed
+  ];
+
+  const SportPicker: React.FC<SportPickerProps> = ({ onSportSelect }) => {
+    return (
+      <div className="sport-picker">
+        {sports.map((sport) => (
+          <button key={`${sport.sport}-${sport.league}`} onClick={() => onSportSelect(sport)}>
+            <img src={`${sport.league}.png`} />
+            {sport.league.toUpperCase()}
+          </button>
+        ))}
+      </div>
+    );
+  };
+
+export default SportPicker;
