6b3f2f47434f07a8d584eb09e9d0e7007a65fd28
jDelille <justind7513@gmail.com>
2024-06-20 18:35:12 -0700

    wip: working on matches

--- frontend/src/pages/Match.tsx
+++ frontend/src/pages/Match.tsx
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import matchStore from "../store/matchStore";
 import { useBetSlip } from "../hooks";
-import "./page.scss";
-import { Price } from "../types/BovadaMatch";
 import MatchCategories from "../components/match-categories/MatchCategories";
+import "./page.scss";
 
 type MatchProps = {};
 
 const Match: React.FC<MatchProps> = () => {
+  const [selectedCategory, setSelectedCategory] = useState("Game Lines");
+
   const match = matchStore.match;
   const displayGroups = match?.displayGroups;
   const betSlip = useBetSlip();
@@ -21,38 +22,47 @@ const Match: React.FC<MatchProps> = () => {
     betSlip.onOpen(bet);
   };
 
+  console.log(displayGroups);
+
   return (
     <div className="page">
-            <p>{match?.description}</p>
+      <p>{match?.description}</p>
 
-      <MatchCategories displayGroups={displayGroups}/>
-
+      <MatchCategories displayGroups={displayGroups} />
 
       {displayGroups?.map((group, index) => {
-        if (index === 0) {
+        if(selectedCategory === group?.description) {
           return (
             <div>
-              <p>{group.description}</p>
-              <p>{group.markets[2].description}</p>
-
-              <div
-                className="outcomes"
-                onClick={() =>
-                  handleClick(
-                    group.markets[2].outcomes[0].description,
-                    group.markets[2].outcomes[0].price.american
-                  )
-                }
-              >
-                <p>{group.markets[2].outcomes[0].description}</p>
-                <p>{group.markets[2].outcomes[0].price.american}</p>
-              </div>
+              <p>{group?.description}</p>
+  
+              {group.markets.map((market) =>
+                market.outcomes.map((outcome) => (
+                  <p className="outcome">{outcome.description}</p>
+                ))
+              )}
             </div>
           );
         }
+        
       })}
     </div>
   );
 };
 
 export default Match;
+
+{
+  /* <div
+            className="outcomes"
+            onClick={() =>
+              handleClick(
+                group?.markets[2]?.outcomes[0].description,
+                group.markets[2].outcomes[0].price.american
+              )
+            }
+          >
+            <p>{group.markets[2].outcomes[0].description}</p>
+            <p>{group.markets[2].outcomes[0].price.american}</p>
+          </div> */
+}
