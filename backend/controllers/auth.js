2ff4c7b1c50e6df0c4d1d6948c65e9c2e62659e0
jDelille <justind7513@gmail.com>
2024-08-30 14:22:36 -0700

    wip: secure access token

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -73,6 +73,8 @@ export const login = (req, res) => {
     res
       .cookie("accessToken", token, {
         httpOnly: true,
+        secure: true,
+        sameSite: 'lax',
       })
       .status(200)
       .json(others);
