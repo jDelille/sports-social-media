6696ff69bccc261d77f9541ea96c493c974438fb
jDelille <justind7513@gmail.com>
2024-09-12 19:29:42 -0700

    fix: changed cookie settings

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -73,7 +73,7 @@ export const login = (req, res) => {
       .cookie("accessToken", token, {
         httpOnly: true, 
         secure: process.env.NODE_ENV === 'production', 
-        sameSite: "Strict", 
+        sameSite: "none", 
       })
       .status(200)
       .json(others);
